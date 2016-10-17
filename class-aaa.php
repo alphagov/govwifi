66e80dd71e6d18e2b1d0c5164d42f12de446f564
afoldesi-gds <attila.foldesi@digital.cabinet-office.gov.uk>
2016-10-17 13:13:54 +0100

    Straight merge of api.php and class aaa from the old master.

--- class-aaa.php
+++ class-aaa.php
@@ -5,14 +5,16 @@ class aaa
     public $user;
     public $mac;
     public $siteIP;
+    public $site;
     public $ap;
     public $type;
     public $responseHeader;
     public $responseBody;
-    public $reqeustJson;
+    public $requestJson;
     public $session;
     public $result;
-    
+    public $kioskKey;
+
     public function __construct($request)
     {
         $parts = explode('/', $request);
@@ -36,9 +38,22 @@ class aaa
                     break;
                 case "site":
                     $this->siteIP = $parts[$x + 1];
+                    $this->site = new site;
+                    $this->site->loadByIp($this->siteIP);
+                    break;
+                case "kioskip":
+                    $this->site = new site;
+                    $this->site->loadByKioskIp($parts[$x + 1]);
                     break;
                 case "result":
                     $this->result = $parts[$x + 1];
+                    break;
+                case "phone":
+                    $this->user = new user;
+                    $this->user->identifier = new identifier($parts[$x + 1]);
+                    break;
+                case "code":
+                    $this->kioskKey = $parts[$x + 1];
                     break;
             }
 
@@ -59,10 +74,49 @@ class aaa
             case "accounting":
                 $this->accounting();
                 break;
+            case "activate":
+                $this->activate();
+                break;
 
         }
 
     }
+    public function kioskKeyValid()
+    {
+        if (strtoupper($this->site->kioskKey) == strtoupper($this->kioskKey)) {
+         return true;
+        }
+         else {
+         return false;
+         error_log($this->site->kioskKey." ".$this->kioskKey);
+         }
+    }
+
+    public function activate()
+    {
+        error_log("Site ID: ".$this->site->id);
+        if (($this->site->id) and $this->kioskKeyValid())
+            {
+            if (isset($this->user->identifier) && $this->user->identifier->validMobile) {
+                // insert an activation entry
+                $this->user->kioskActivate($this->site->id);
+                $this->user->loadRecord();
+                if (!$this->user->login) {
+                     $sms = new smsResponse;
+                     $sms->to = $this->user->identifier->text;
+                     $sms->setReply();
+                     $sms->terms();
+                }
+            }
+            else
+            print $this->site->getDailyCode();
+        }
+        else
+        {
+               $this->responseHeader = "404 Not Found";
+        }
+    }
+
     public function accounting()
     {
         $acct = json_decode($this->requestJson, true);
@@ -147,37 +201,63 @@ class aaa
         }
 
     }
-    
+
     private function fixMac($mac) {
         //convert to upper case
         $mac = strtoupper($mac);
         //get rid of anything that isn't hex
         $mac = preg_replace('/[^0-F]/',"",$mac);
-        // recreate the mac in IETF format using the first 12 chars. 
+        // recreate the mac in IETF format using the first 12 chars.
         $mac = substr($mac,0,2).'-'.substr($mac,2,2).'-'.substr($mac,4,2).'-'.substr($mac,6,2).'-'.substr($mac,8,2).'-'.substr($mac,10,2);
-        return $mac;       
+        return $mac;
     }
-    
+
     public function setMac($mac) {
         $this->mac=$this->fixMac($mac);
     }
     public function setAp($mac) {
         $this->ap=$this->fixMac($mac);
     }
+
     public function authorize()
     {
-        if ($this->user->identifier->text)
-        {
-            $this->responseHeader = "HTTP/1.0 200 OK";
-            $response['control:Cleartext-Password'] = $this->user->password;
-            $this->responseBody = json_encode($response);
-        } else
-        {
-            $this->responseHeader = "HTTP/1.0 401 Forbidden";
+    // If this matches a user account continue
+        if (isset($this->user->identifier)
+            && $this->user->identifier->text) {
+            // If the site isn't restricted
+            if (($this->site->activationRegex == "")
+            // or the user's email address is authorised
+            or preg_match('/'.$this->site->activationRegex.'/', $this->user->email))
+            {
+                $this->authorizeResponse(TRUE);
+            }
+            else
+            {
+                error_log("Restricted site: ".$this->site->activationRegex." Users email: ".$this->user->email);
+                // or the user has activated at this site
+            if ($this->user->activatedHere($this->site))
+                {
+                    $this->authorizeResponse(TRUE);
+                } else {
+                    $this->authorizeResponse(FALSE);
 
+                }
+            }
         }
-
     }
+
+
+    private function authorizeResponse($accept)
+        {
+        if ($accept) {
+                $this->responseHeader = "200 OK";
+                $response['control:Cleartext-Password'] = $this->user->password;
+                $this->responseBody = json_encode($response);
+            }
+            else {
+                 $this->responseHeader = "404 Not Found";
+            }
+        }
 }
 
 ?>
