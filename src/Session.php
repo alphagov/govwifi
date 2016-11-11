c4e4e548e7d830ff6b22f328c5b8328b562b7c51
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2016-11-11 14:40:44 +0000

    Fix session table name.

--- src/Session.php
+++ src/Session.php
@@ -68,7 +68,7 @@ class Session {
         $db = DB::getInstance();
         $dblink = $db->getConnection();
         $handle = $dblink->prepare(
-                "update sessions set stop=now(), inMB=:inMB, outMB=:outMB "
+                "update session set stop=now(), inMB=:inMB, outMB=:outMB "
                 . "where siteIP=:siteIP and username=:username "
                 . "and stop is null and mac=:mac and ap=:ap "
                 . "and start between :startmin and :startmax");
