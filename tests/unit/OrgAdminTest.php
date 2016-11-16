d3a8c884229b9ad3332d39de68418881b9be751f
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2016-11-16 15:58:28 +0000

    Try db setup with orgAdmin test.

--- tests/unit/OrgAdminTest.php
+++ tests/unit/OrgAdminTest.php
@@ -5,7 +5,6 @@ use PHPUnit_Framework_TestCase;
 
 class OrgAdminTest extends PHPUnit_Framework_TestCase {
     function testClassInstantiates() {
-        // TODO: This requires the test database setup.
-        //$this->assertInstanceOf(OrgAdmin::class, new OrgAdmin("test@test.com"));
+        $this->assertInstanceOf(OrgAdmin::class, new OrgAdmin("test@test.com"));
     }
 }
