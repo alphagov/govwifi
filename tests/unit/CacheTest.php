a570f5b8a5f8b74fe75c8eca6c531c17bd16a1c0
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2016-11-18 15:16:03 +0000

    Tests for Cache.

--- tests/unit/CacheTest.php
+++ tests/unit/CacheTest.php
@@ -4,7 +4,24 @@ namespace Alphagov\GovWifi;
 use PHPUnit_Framework_TestCase;
 
 class CacheTest extends PHPUnit_Framework_TestCase {
+    const KEY = "testSessionID";
+    const DATA = array("test1" => "val1", "test2" => 42);
+
     function testClassInstantiates() {
         $this->assertInstanceOf(Cache::class, Cache::getInstance());
+    }
+
+    function testValueRetrieval() {
+        $cache = Cache::getInstance();
+        $this->assertTrue($cache->set(self::KEY, self::DATA));
+        $this->assertEquals(self::DATA, $cache->get(self::KEY));
+    }
+
+    function testValueRemoval() {
+        $cache = Cache::getInstance();
+        $this->assertTrue($cache->set(self::KEY, self::DATA));
+        $this->assertTrue($cache->delete(self::KEY));
+        $this->assertFalse($cache->get(self::KEY));
+        $this->assertTrue($cache->itemWasNotFound());
     }
 }
