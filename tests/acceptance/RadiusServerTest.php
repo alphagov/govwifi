c68a2a943f021d3f816a8da73e4e7f32ccbe7d58
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2016-11-23 14:58:18 +0000

    Test for radius health check authentication.

--- tests/acceptance/RadiusServerTest.php
+++ tests/acceptance/RadiusServerTest.php
@@ -1,9 +1,12 @@
 <?php
 
+use Alphagov\GovWifi\Config;
+
 class RadiusServerTest extends PHPUnit_Framework_TestCase {
     const PEAP_MSCHAP_V2_CONFIG_TEMPLATE = "tests/acceptance/config/peap-mschapv2.conf";
-    const CONFIG_FILE = "tests/acceptance/config/currentconfig.conf";
-    const EAPOL_TEST_RUNNER = "tests/acceptance/config/run-eapol-test.sh";
+    const CONFIG_FILE                    = "tests/acceptance/config/currentconfig.conf";
+    const EAPOL_TEST_RUNNER              = "tests/acceptance/config/run-eapol-test.sh";
+    const HEALTH_CHECK_USER_PASSWORD     = 'GS3EWA64EshRD8I0XdVl$dko';
     /**
      * @var string The user name to authenticate with.
      */
@@ -19,10 +22,26 @@ class RadiusServerTest extends PHPUnit_Framework_TestCase {
         $this->password  = getenv("TEST_USER_PASSWORD");
     }
 
+    /**
+     * @coversNothing Do not count acceptance tests in code coverage analysis.
+     */
     public function testRadiusPEAPMsChapV2Authentication() {
         $template = file_get_contents(self::PEAP_MSCHAP_V2_CONFIG_TEMPLATE);
         $configuration = str_replace("#PASSWORD#", $this->password,
             str_replace("#USERNAME#", $this->userName, $template)
+        );
+        file_put_contents(self::CONFIG_FILE, $configuration);
+
+        $this->assertEquals("SUCCESS", exec("/bin/bash " . self::EAPOL_TEST_RUNNER));
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testRadiusHealthCheckAuthentication() {
+        $template = file_get_contents(self::PEAP_MSCHAP_V2_CONFIG_TEMPLATE);
+        $configuration = str_replace("#PASSWORD#", self::HEALTH_CHECK_USER_PASSWORD,
+            str_replace("#USERNAME#", Config::HEALTH_CHECK_USER, $template)
         );
         file_put_contents(self::CONFIG_FILE, $configuration);
 
