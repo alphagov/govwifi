c83b3beddd35a7b1d715d6bf46cacf98518cad8e
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2016-11-30 10:49:36 +0000

    Gathered examples, removing test log.

--- src/api/api.php
+++ src/api/api.php
@@ -6,8 +6,6 @@ require "../common.php";
 $jsonData = file_get_contents('php://input');
 $aaa = new AAA($_SERVER['SCRIPT_NAME'], $jsonData);
 $aaa->processRequest();
-// TODO: This is just to gather examples for automated testing. Remove when done.
-error_log("Request JSON: >>>>" . $jsonData . "<<<<");
 
 header($_SERVER["SERVER_PROTOCOL"].' '.$aaa->responseHeader);
 header("Content-Type: application/json");
