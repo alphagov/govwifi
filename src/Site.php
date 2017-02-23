d6493ecb61b2c8ad4d5d499749dbcd75542fe692
Samuel L <admin@preprocess.uk>
2017-02-23 07:55:38 +0000

    Added spaces

--- src/Site.php
+++ src/Site.php
@@ -83,10 +83,10 @@ class Site {
     }
 
     public function attributesText() {
-        $attributes = "Postcode: " . $this->postcode."\n";
-        $attributes .= "Activation-whitelist: " . $this->getWhitelist()."\n";
-        $attributes .= "Activation-days: " . $this->activationDays."\n";
-        $attributes .= "DataController: " . $this->dataController."\n";
+        $attributes = "Postcode: " . $this->postcode . "\n";
+        $attributes .= "Activation-whitelist: " . $this->getWhitelist() . "\n";
+        $attributes .= "Activation-days: " . $this->activationDays . "\n";
+        $attributes .= "DataController: " . $this->dataController . "\n";
         return $attributes;
     }
 
@@ -96,32 +96,32 @@ class Site {
             $line = str_replace(">", "", $line);
             $line = str_replace("*", "", $line);
             $line = trim($line);
-            $parameter = strtolower(trim(substr($line, 0, strpos($line,":"))));
+            $parameter = strtolower(trim(substr($line, 0, strpos($line, ":"))));
 
-            $value = substr($line, strpos($line,":")+1);
+            $value = substr($line, strpos($line, ":") + 1);
 
             switch ($parameter) {
                 case "postcode":
-                    error_log("*".$parameter."*");
+                    error_log("*" . $parameter . "*");
                     $this->postcode = $value;
                     $updated = true;
                     break;
                 case "activation-whitelist":
-                    error_log("*".$parameter."*");
-                    $value = str_replace(" ","",$value);
-                    $value = str_replace(",","$|",$value);
+                    error_log("*" . $parameter . "*");
+                    $value = str_replace(" ", "", $value);
+                    $value = str_replace(",", "$|", $value);
                     $value .="$";
-                    error_log("activation_regex:/".$value."/");
+                    error_log("activation_regex:/" . $value . "/");
                     $this->activationRegex = $value;
                     $updated = true;
                     break;
                 case "activation-days":
-                    error_log("*".$parameter."*");
+                    error_log("*" . $parameter . "*");
                     $this->activationDays = $value;
                     $updated = true;
                     break;
                 case "datacontroller":
-                    error_log("*".$parameter."*");
+                    error_log("*" . $parameter . "*");
                     $this->dataController = $value;
                     $updated = true;
                     break;
