2b3313e49ae7f2c67b9ceac8fd1e480f2808f480
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2017-03-29 06:48:08 +0100

    Call survey if key matches.

--- src/timedjobs/survey/index.php
+++ src/timedjobs/survey/index.php
@@ -5,5 +5,9 @@ require "../../common.php";
 
 
 if (Config::getInstance()->values["frontendApiKey"] == $_REQUEST['key']) {
-
+    $survey = new Survey([
+        'config' => Config::getInstance(),
+        'db'     => DB::getInstance()
+    ]);
+    $survey->sendSurveys();
 }
