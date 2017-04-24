0815f7197b538bed0098ab073927c7822ff08ec9
Samuel L <admin@preprocess.uk>
2017-04-24 15:37:56 +0100

    Fix code style
    - `if (! ...`
    - Removed blank line
    - Changed `==` to `===`

--- src/timedjobs/performanceplatform/index.php
+++ src/timedjobs/performanceplatform/index.php
@@ -6,11 +6,10 @@ require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "common.php";
 use DateInterval;
 use DateTime;
 
-if (!empty($_REQUEST['key']) && Config::getInstance()->values["frontendApiKey"] == $_REQUEST['key']) {
-
+if (! empty($_REQUEST['key']) && Config::getInstance()->values["frontendApiKey"] === $_REQUEST['key']) {
     $reportVolumetrics = new ReportVolumetrics(Config::getInstance(), DB::getInstance());
 
-    if (!empty($_REQUEST['days']) && is_numeric($_REQUEST['days'])) {
+    if (! empty($_REQUEST['days']) && is_numeric($_REQUEST['days'])) {
         for ($i = $_REQUEST['days']; $i >= 1; $i--) {
             $dateObject = new DateTime();
             $reportVolumetrics->sendMetrics(
