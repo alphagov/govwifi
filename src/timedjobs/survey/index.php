ef01ba2a42637f29161ddff28dfd8406a2ffe33e
Samuel L <admin@preprocess.uk>
2017-04-24 15:34:27 +0100

    Fix code style
    - `if (! ...`
    - Removed blank line
    - Changed `==` to `===`

--- src/timedjobs/survey/index.php
+++ src/timedjobs/survey/index.php
@@ -3,8 +3,7 @@ namespace Alphagov\GovWifi;
 
 require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "common.php";
 
-
-if (!empty($_REQUEST['key']) && Config::getInstance()->values["frontendApiKey"] == $_REQUEST['key']) {
+if (! empty($_REQUEST['key']) && Config::getInstance()->values["frontendApiKey"] === $_REQUEST['key']) {
     $survey = new Survey([
         'config' => Config::getInstance(),
         'db'     => DB::getInstance()
